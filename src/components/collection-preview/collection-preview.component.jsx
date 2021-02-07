fe0609229302e42504ee28eef1813a4a457a9311
Duy <huynhduytam2210@gmail.com>
2021-02-07 20:56:49 +0700

    Collection Item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { CollectionItem } from '../collection-item/collection-item.component'
+import './collection-preview.styles.scss'
 
 export const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
@@ -6,8 +8,8 @@ export const CollectionPreview = ({ title, items }) => (
         <div className='preview'>
             {
                 items.filter((item, index) => index < 4)
-                .map(item => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
         </div>
