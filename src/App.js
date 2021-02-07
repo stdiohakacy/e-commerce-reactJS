841fbd3161b312fc4056266fcccd06f24a874a44
Duy <huynhduytam2210@gmail.com>
2021-02-07 21:58:10 +0700

    Sign In Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { ShopPage } from './pages/shop/shop.component';
 import { Header } from './components/header/header.component';
+import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup.component'
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route path='/shop' component={ShopPage} />
+                <Route path='/signin' component={SignInAndSignUp} />
             </Switch>
         </div>
     );
