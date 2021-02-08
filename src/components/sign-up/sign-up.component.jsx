aadab9500f5abfa2d758610db8e9f5c92655b857
Duy <huynhduytam2210@gmail.com>
2021-02-08 21:38:04 +0700

    Sign Up With Email and Password

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -24,7 +24,7 @@ export class SignUp extends Component {
         }
 
         try {
-            const { user } = await auth.createUserWithEmailAndPassword({ email, password })
+            const { user } = await auth.createUserWithEmailAndPassword(email, password)
 
             await createUserProfileDocument(user, { displayName })
 
