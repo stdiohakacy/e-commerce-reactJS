6c9b4d3223810255209a1c3a9c63adbb558a27d1
Duy <huynhduytam2210@gmail.com>
2021-02-09 13:52:49 +0700

    Implementing Redux In Cart

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { HomePage } from './pages/homepage/homepage.component';
 import './App.css';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { ShopPage } from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.util'
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.action';
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import CartIcon from '../cart-icon/cart-icon.component'
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -24,12 +24,16 @@ const Header = ({ currentUser }) => (
                 : (<Link className='option' to='/signin'>SIGN IN</Link>)}
             <CartIcon />
         </div>
-        <CartDropdown />
+        {hidden ? null : <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+// const mapStateToProps = state => ({
+//     currentUser: state.user.currentUser
+// })
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header)
