45b22967d86ff74a99f847ac9942b1ed84ced7e6
Duy <huynhduytam2210@gmail.com>
2021-02-11 12:25:39 +0700

    Data Flow In Our AppD

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,12 +1,12 @@
 import { createSelector } from "reselect"
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
+// const COLLECTION_ID_MAP = {
+//     hats: 1,
+//     sneakers: 2,
+//     jackets: 3,
+//     womens: 4,
+//     mens: 5
+// }
 
 const selectShop = state => state.shop
 
@@ -28,3 +28,8 @@ export const selectCollection = collectionUrlParam => {
         }
     )
 }
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
